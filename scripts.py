76b659e3f3781c65321af27508001f450e4d7983
briana.castilla <briana.castilla@gmail.com>
2026-01-13 12:29:02 +0100

    tried my best

--- scripts.py
+++ scripts.py
@@ -6,7 +6,6 @@ import json
 with open('ucaccmet2j_python/precipitation.json') as file:
     data= json.load(file)
 
-print(data)
 
 # Manually looking up seattle code
 # US1WAKG0038
@@ -16,6 +15,33 @@ seattle= []
 
 for measurement in data:
     if measurement["station"]=="GHCND:US1WAKG0038":
-        print(measurement)
         seattle.append(measurement)
 # added
+
+#print(seattle)
+
+# total monthly precipitation 
+# creating a list with the total preci x month, 
+# add every day of the month into a month value
+# and a list with those month values
+
+
+totalmonthly = {}
+#months= ['01','02','03', '03','04','05','06','07','08','09','10','11','12']
+
+
+for measurement in seattle:
+    date = measurement['date']
+    month = date.split('-')[1]
+        
+    if month in totalmonthly:
+        totalmonthly['month'] += measurement['value']
+    else:
+        totalmonthly['month'] = measurement['value']
+           
+print(totalmonthly)
+
+# import json
+
+# with open('results.json', 'w', encoding='utf-8') as file:
+#     json.dump(totalmonthly, file, indent=4)
