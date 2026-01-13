383ab5988003ab380d6afa0a867f3bf32d44cedd
briana.castilla <briana.castilla@gmail.com>
2026-01-13 15:06:49 +0100

    Done, but does not save everything, just what user asked

--- scripts.py
+++ scripts.py
@@ -12,14 +12,32 @@ with open('ucaccmet2j_python/precipitation.json') as file:
 # US1WAKG0038
 # creating a new dict with only seattle values
 
-seattle= []
+# now changing not only to seattle but to option user wants
+
+ask=input("Which city? Cincinnati=1, Seattle=2, Maui=3, San Diego=4 ")
+
+if ask== '1':
+    code= "GHCND:USW00093814"
+    name= "Cincinnati"
+elif ask== '2':
+    code= "GHCND:US1WAKG0038"
+    name= "Seattle"
+elif ask== '3':
+    code= "GHCND:USC00513317"
+    name= "Maui"
+elif ask=='4':
+    code= 'GHCND:US1CASD0032'
+    name= 'San Diego'
+else:
+    exit()
+
+city= []
+print(name)
 
 for measurement in data:
-    if measurement["station"]=="GHCND:US1WAKG0038":
-        seattle.append(measurement)
+    if measurement["station"]==(f"{code}"):
+        city.append(measurement)
 # added
-
-#print(seattle)
 
 # total monthly precipitation 
 # creating a list with the total preci x month, 
@@ -32,10 +50,10 @@ totalmonthly = {}
 # but it actually didn't
 # so then eduardo helped me #thankssomuch
 
-print("Monthly precipitation for the area in units")
+print(f"Monthly precipitation for {name} in units")
 print()
 
-for measurement in seattle:
+for measurement in city:
     date = measurement['date']
     month = date.split('-')[1]
         
@@ -60,20 +78,21 @@ with open('ucaccmet2j_python/results.json', 'w', encoding='utf-8') as file:
 # Calculate total yearly preci, 
 # the sum of preci in a year for location
 
-print("Total yearly precipitation for the area")
+print(f"Total yearly precipitation for {name}")
 
 # this could be done just with total monthly but oh well
 totalyearly = 0
-for measurement in seattle:
+for measurement in city:
     totalyearly += measurement['value']
 print(totalyearly)
 
 print()
+
 # relative monthly precipitation
 # per month
 # dictionary, key = month, value = percentage 
 
-print("Relative monthly precipitation for the area as a percentage of yearly total")
+print(f"Relative monthly precipitation for {name} as a percentage of yearly total")
 print()
 
 relativemonthly = {}
