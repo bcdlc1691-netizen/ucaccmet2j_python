aad47dea84fb0fe4eeea4c27b75be600a6bdd07e
briana.castilla <briana.castilla@gmail.com>
2026-01-13 17:28:59 +0100

    commit soon to be done

--- scripts.py
+++ scripts.py
@@ -7,110 +7,152 @@ import json
 with open('ucaccmet2j_python/precipitation.json') as file:
     data= json.load(file)
 
-
 # Manually looking up seattle code
 # US1WAKG0038
 # creating a new dict with only seattle values
 
 # now changing not only to seattle but to option user wants
+# now to every single thing. like ever
+# GIANT for loop, let us try
 
-ask=input("Which city? Cincinnati=1, Seattle=2, Maui=3, San Diego=4 ")
+listofcitynames={"Cincinnati", "Seattle", 'Maui', 'San Diego'}
 
-if ask== '1':
-    code= "GHCND:USW00093814"
-    name= "Cincinnati"
-elif ask== '2':
-    code= "GHCND:US1WAKG0038"
-    name= "Seattle"
-elif ask== '3':
-    code= "GHCND:USC00513317"
-    name= "Maui"
-elif ask=='4':
-    code= 'GHCND:US1CASD0032'
-    name= 'San Diego'
-else:
-    exit()
-
-city= []
-print(name)
-
-for measurement in data:
-    if measurement["station"]==(f"{code}"):
-        city.append(measurement)
-# added
-
-# total monthly precipitation 
-# creating a list with the total preci x month, 
-# add every day of the month into a month value
-# and a list with those month values
-
-
-totalmonthly = {}
-# I was doing it a very different way that apparently worked according to both TAs
-# but it actually didn't
-# so then eduardo helped me #thankssomuch
-
-print(f"Monthly precipitation for {name} in units")
-print()
-
-for measurement in city:
-    date = measurement['date']
-    month = date.split('-')[1]
+for cityname in listofcitynames:
+    if cityname== 'Cincinnati':
+        code= "GHCND:USW00093814"
         
-    if month in totalmonthly:
-        totalmonthly[month] += measurement['value']
+    elif cityname== 'Seattle':
+        code= "GHCND:US1WAKG0038"
+    
+    elif cityname== 'Maui':
+        code= "GHCND:USC00513317"
+
+    elif cityname=='San Diego':
+        code= 'GHCND:US1CASD0032'
+    
     else:
-        totalmonthly[month] = measurement['value']
-           
-print(totalmonthly)
+        exit()
 
-print()
-
-# save to json
-
-import json
-
-with open('ucaccmet2j_python/results.json', 'w', encoding='utf-8') as file:
-    json.dump(totalmonthly, file, indent=4)
-
-## plot bonus did not work, continue
-
-# Calculate total yearly preci, 
-# the sum of preci in a year for location
-
-print(f"Total yearly precipitation for {name}")
-
-# this could be done just with total monthly but oh well
-totalyearly = 0
-for measurement in city:
-    totalyearly += measurement['value']
-print(totalyearly)
-
-print()
-
-# relative monthly precipitation
-# per month
-# dictionary, key = month, value = percentage 
-
-print(f"Relative monthly precipitation for {name} as a percentage of yearly total")
-print()
-
-relativemonthly = {}
-for month in totalmonthly.values():
-    relativemonthly[month] = (month/ totalyearly) * 100
-    relativemonthly[month] = round(relativemonthly[month], 2)
-
-listrm= (list(relativemonthly.values()))
-print(listrm)
-
-print()
-
-# save to alr existing json file!
-
-with open('ucaccmet2j_python/results.json', "a", encoding='utf-8') as file:
-    json.dump(listrm, file, indent=4)
-
-# Intercity rains
-# ask user for which station they want?
+    city= []
 
 
+    for measurement in data:
+        if measurement["station"]==(f"{code}"):
+            city.append(measurement)
+    # added
+
+    # total monthly precipitation 
+    # creating a list with the total preci x month, 
+    # add every day of the month into a month value
+    # and a list with those month values
+
+
+    totalmonthly = {}
+    # I was doing it a very different way that apparently worked according to both TAs
+    # but it actually didn't
+    # so then eduardo helped me #thankssomuch
+
+    print(f"Monthly precipitation for {cityname} in units")
+    print()
+
+    for measurement in city:
+        date = measurement['date']
+        month = date.split('-')[1]
+            
+        if month in totalmonthly:
+            totalmonthly[month] += measurement['value']
+        else:
+            totalmonthly[month] = measurement['value']
+            
+    print(totalmonthly)
+
+    print()
+
+    # save to json
+
+    ## plot bonus did not work, continue
+
+    # Calculate total yearly preci, 
+    # the sum of preci in a year for location
+
+    print(f"Total yearly precipitation for {cityname}")
+
+    # this could be done just with total monthly but oh well
+    totalyearly = 0
+    for measurement in city:
+        totalyearly += measurement['value']
+    print(totalyearly)
+
+    print()
+
+    # relative monthly precipitation
+    # per month
+    # dictionary, key = month, value = percentage 
+
+    print(f"Relative monthly precipitation for {cityname} as a percentage of yearly total")
+    print()
+
+    relativemonthly = {}
+    for month in totalmonthly.values():
+        relativemonthly[month] = (month/ totalyearly) * 100
+        relativemonthly[month] = round(relativemonthly[month], 2)
+
+    listrm= (list(relativemonthly.values()))
+    print(listrm)
+
+    print()
+
+    # save to alr existing json file!
+
+
+    # Intercity rains
+    # ask user for which station they want?
+    # now i have to save everything to the json
+    # first solve json line problem that pops up 
+    # done
+    # i want to save everything I printed to json
+    # does not work
+
+    # import json
+
+    # with open('ucaccmet2j_python/results.json', 'w', encoding='utf-8') as file:
+    #     json.dump(totalmonthly, file, indent=4)
+    # with open('ucaccmet2j_python/results.json', 'a', encoding='utf-8') as file:
+    #     json.dump(listrm, file, indent=4)
+# should be in this form
+
+
+# finaldata= {
+    # "Cincinnati": {
+    # "station": "GHCND: ",
+    # "state": " ",
+    # "total_monthly_precipitation": [...],
+    # "total_yearly_precipitation": ...,
+    # "relative_monthly_precipitation": [...],
+    # "relative_yearly_precipitation": ...
+    # }
+    # "Seattle": {
+    # "station": "GHCND: ",
+    # "state": " ",
+    # "total_monthly_precipitation": [...],
+    # "total_yearly_precipitation": ...,
+    # "relative_monthly_precipitation": [...],
+    # "relative_yearly_precipitation": ...
+    # }
+    # "Maui": {
+    # "station": "GHCND:",
+    # "state": " ",
+    # "total_monthly_precipitation": [...],
+    # "total_yearly_precipitation": ...,
+    # "relative_monthly_precipitation": [...],
+    # "relative_yearly_precipitation": ...
+    # }
+    # 'San Diego': {
+    # "station": "GHCND:",
+    # "state": " ",
+    # "total_monthly_precipitation": [...],
+    # "total_yearly_precipitation": ...,
+    # "relative_monthly_precipitation": [...],
+    # "relative_yearly_precipitation": ...
+    # }
+# }
